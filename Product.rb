73e1ed8434af3c2583df316e6202b294aa7171a2
Shunta-Kogen <shuntakgn@gmail.com>
2022-02-03 23:00:19 +0900

    2022/2/3

--- Product.rb
+++ Product.rb
@@ -1,3 +1,5 @@
+# 問題文 https://atcoder.jp/contests/abs/tasks/abc086_a
+
 a, b = gets.chomp.split(" ").map(&:to_i)
 
 if (a * b).even?
--- Welcome_to_AtCoder.rb
+++ Welcome_to_AtCoder.rb
@@ -1,3 +1,5 @@
+# 問題 https://atcoder.jp/contests/abs/tasks/practice_1
+
 a = gets.to_i
 b, c = gets.split(" ").map(&:to_i)
 s = gets.chomp
