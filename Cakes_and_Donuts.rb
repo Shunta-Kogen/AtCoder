59ae097252ac808795b63f7a84abb5e4fe165386
Shunta-Kogen <shuntakgn@gmail.com>
2022-02-12 23:59:09 +0900

    2022/2/12

--- Cakes_and_Donuts.rb
+++ Cakes_and_Donuts.rb
@@ -1,9 +1,11 @@
 # 問題文 https://atcoder.jp/contests/abc105/tasks/abc105_b
 
 n = gets.to_i
+max_cakes = 100 / 4
+max_donuts = 100 / 7
 
-(0..100).each do |i|
-  (0..100).each do |j|
+(0..max_cakes).each do |i|
+  (0..max_donuts).each do |j|
     if 4 * i + 7 * j == n
       puts "YES"
       exit
