5a07a879bff8eaa68a4ae505748a791d4bc3cb3a
Shunta-Kogen <shuntakgn@gmail.com>
2022-02-16 23:51:52 +0900

    2022/2/16

--- Palace.rb
+++ Palace.rb
@@ -4,10 +4,10 @@ n = gets.to_i
 t, a = gets.split(" ").map(&:to_i)
 h = gets.split(" ").map(&:to_i)
 
-list = []
+point_list = []
 h.each do |i|
   avg_temperature = t - i * 0.006
-  list << (avg_temperature - a).abs
+  point_list << (avg_temperature - a).abs
 end
 
 puts list.index(list.min) + 1
