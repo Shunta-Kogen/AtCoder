fe7e4da4f59ce52a2c792f1d861097b2693bf806
Shunta-Kogen <shuntakgn@gmail.com>
2022-03-01 00:58:30 +0900

    2022/2/28

--- Pasta.rb
+++ Pasta.rb
@@ -1,3 +1,17 @@
 # 問題文 https://atcoder.jp/contests/abc241/tasks/abc241_b
 
 n, m = gets.split.map(&:to_i)
+a = gets.split.map(&:to_i)
+b = gets.split.map(&:to_i)
+result = "Yes"
+
+b.each do |e|
+  delete_index = a.index(e)
+  if delete_index == nil
+    result = "No"
+    break
+  end
+  a.delete_at(delete_index)
+end
+
+puts result
